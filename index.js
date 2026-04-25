597251b643f9f992757d887461d41ed4d6888108
joey <54230107@qq.com>
2026-04-26 06:29:02 +0800

    v3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     $('head')
-        .append('<link type="text/css" rel="stylesheet" href="./css/iconfont.css" id="handyUiLoaded">')
-        .append('<link type="text/css" rel="stylesheet" href="./css/handy-ui.css">')
-    $.getScript('./scripts/common.js')
+        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/iconfont.css" id="handyUiLoaded">')
+        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/handy-ui.css">')
+    $.getScript('https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/scripts/common.js')
 }
 
