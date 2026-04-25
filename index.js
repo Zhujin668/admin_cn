44ae5526d653acb37057747fdda10ef883b2ee00
joey <54230107@qq.com>
2026-04-26 06:05:49 +0800

    v2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     $('head')
-        .append('<link type="text/css" rel="stylesheet" href="resources/handy-ui/css/iconfont.css" id="handyUiLoaded">')
-        .append('<link type="text/css" rel="stylesheet" href="resources/handy-ui/css/handy-ui.css">')
-
-    $.getScript('./resources/handy-ui/scripts/common.js')
+        .append('<link type="text/css" rel="stylesheet" href="./css/iconfont.css" id="handyUiLoaded">')
+        .append('<link type="text/css" rel="stylesheet" href="./css/handy-ui.css">')
+    $.getScript('./scripts/common.js')
 }
 
