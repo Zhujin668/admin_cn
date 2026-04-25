8fe3e51470dc3bb9bcbd54eb1ab204906281abe6
joey <54230107@qq.com>
2026-04-26 06:43:28 +0800

    v5

--- admin.js
+++ admin.js
@@ -1,12 +1,12 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     $('head')
-        .append('<link type="text/css" rel="stylesheet" href="resources/handy-ui/css/iconfont.css" id="handyUiLoaded">')
-        .append('<link type="text/css" rel="stylesheet" href="resources/handy-ui/css/handy-ui.css">')
-        .append('<link type="text/css" rel="stylesheet" href="resources/handy-ui/css/portal.css">')
+        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/iconfont.css" id="handyUiLoaded">')
+        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/handy-ui.css">')
+        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/portal.css">')
 
-    $.getScript('./resources/handy-ui/scripts/common.js', () => {
-        $.getScript('./resources/handy-ui/scripts/portal.js')
+    $.getScript('https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/scripts/common.js', () => {
+        $.getScript('https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/scripts/portal.js')
     })
 }
 
