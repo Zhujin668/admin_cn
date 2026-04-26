29cd4dbdce1b9216a6349f2a2457947506b02588
joey <54230107@qq.com>
2026-04-26 09:39:18 +0800

    v8

--- admin.js
+++ admin.js
@@ -1,12 +1,12 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     $('head')
-        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/iconfont.css" id="handyUiLoaded">')
-        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/handy-ui.css">')
-        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/portal.css">')
+        .append('<link type="text/css" rel="stylesheet" href="https://admin-cn.pages.dev/css/iconfont.css" id="handyUiLoaded">')
+        .append('<link type="text/css" rel="stylesheet" href="https://admin-cn.pages.dev/css/handy-ui.css">')
+        .append('<link type="text/css" rel="stylesheet" href="https://admin-cn.pages.dev/css/portal.css">')
 
-    $.getScript('https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/scripts/common.js', () => {
-        $.getScript('https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/scripts/portal.js')
+    $.getScript('https://admin-cn.pages.dev/scripts/common.js', () => {
+        $.getScript('https://admin-cn.pages.dev/scripts/portal.js')
     })
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     $('head')
-        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/iconfont.css" id="handyUiLoaded">')
-        .append('<link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/css/handy-ui.css">')
-    $.getScript('https://cdn.jsdelivr.net/gh/Zhujin668/admin_cn@main/scripts/common.js')
+        .append('<link type="text/css" rel="stylesheet" href="https://admin-cn.pages.dev/css/iconfont.css" id="handyUiLoaded">')
+        .append('<link type="text/css" rel="stylesheet" href="https://admin-cn.pages.dev/css/handy-ui.css">')
+    $.getScript('https://admin-cn.pages.dev/scripts/common.js')
 }
 
-console.log(`another test`)
