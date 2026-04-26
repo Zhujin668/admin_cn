098c775508b6a7a1ae4986c4bb24c106f2a6c889
joey <54230107@qq.com>
2026-04-26 10:50:33 +0800

    v11

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     const path = location.search.includes(`env=dev`) ? `./resources/handy-ui` : `https://admin-cn.pages.dev`
+    console.log(location, '<== location')
     $('head')
         .append(`<link type="text/css" rel="stylesheet" href="${path}/css/iconfont.css" id="handyUiLoaded">`)
         .append(`<link type="text/css" rel="stylesheet" href="${path}/css/handy-ui.css">`)
