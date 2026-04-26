295bd637461399621be10b19ed8c8a38f4c1b804
joey <54230107@qq.com>
2026-04-27 03:56:09 +0800

    v26

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
-    const path = location.hash.includes(`env=dev`) ? `./resources/handy-ui` : `https://admin-cn.pages.dev`
+    let path, hostname = location.hostname
+    if (hostname.includes('127.0.0.1') || hostname.includes('localhost')) {
+        path = `./resources/handy-ui`
+    } else {
+        path = `https://admin-cn.pages.dev`
+    }
+    //const path = location.hash.includes(`env=dev`) ? `./resources/handy-ui` : `https://admin-cn.pages.dev`
     $('head')
         .append(`<link type="text/css" rel="stylesheet" href="${path}/css/iconfont.css" id="handyUiLoaded">`)
         .append(`<link type="text/css" rel="stylesheet" href="${path}/css/handy-ui.css">`)
