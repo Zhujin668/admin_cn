fb1f449603dda14212052d16652f40eb63f3c944
joey <54230107@qq.com>
2026-04-27 04:25:35 +0800

    v27

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const loaded = $('#handyUiLoaded')
 if (!(loaded && loaded.length > 0)) {
     let path, hostname = location.hostname
     if (hostname.includes('127.0.0.1') || hostname.includes('localhost')) {
+        console.log('go local')
         path = `./resources/handy-ui`
     } else {
         path = `https://admin-cn.pages.dev`
